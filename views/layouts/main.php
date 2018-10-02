663fcbab48a9b2292a098cbe4692ad268d1ae72f
Akmal Kadirov <akmal@kadirov.org>
2018-10-02 15:59:01 +0300

    view (layout/main) add google analytics block

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -16,6 +16,16 @@ AppAsset::register($this);
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-46483325-6"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-46483325-6');
+    </script>
+
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
